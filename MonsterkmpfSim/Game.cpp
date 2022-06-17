87cf83900af5ca0499c8eb1e5cb1d84e30b9af6b
ncras04 <nicolasaschmitt@googlemail.com>
2022-06-17 12:11:08 +0200

    Unentschieden-Abfrage refactored

--- MonsterkmpfSim/Game.cpp
+++ MonsterkmpfSim/Game.cpp
@@ -2,26 +2,22 @@
 
 int Game::GameInit()
 {
-
 		std::cout << "Welcome to Monsterkampf Simulator!";
 		std::cin.get();
 
-	do
-	{
 		m_monster_1 = Monster::CreateMonster("erstes");
 		m_monster_2 = Monster::CreateMonster("zweites");
 
-		if (CheckDrawPossibility())
+		while (CheckDrawPossibility())
 		{
-			std::cout << "Das Spiel wird in einem Unentschieden enden,"
+			std::cout << "\nDas Spiel wird in einem Unentschieden enden,"
 				"da beide Monster gleich stark sind.";
 			std::cin.get();
 			system("cls");
-		}
-		else
-			break;
 
-	} while (true);
+			m_monster_1 = Monster::CreateMonster("erstes");
+			m_monster_2 = Monster::CreateMonster("zweites");
+		}
 
 	SortBattleOrder();
 
